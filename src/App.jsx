e1a8e0457d10b13bc0a60556c5d73370b8b4534b
Sabin Prajapati <sabinprajapati7@gmail.com>
2025-09-17 00:33:45 +0545

    Tailwind CSs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div>Hello! I am Nabin</div>;
+  return <div className="text-2xl font-bold">Hello! I am Nabin</div>;
 };
 
 export default App;
